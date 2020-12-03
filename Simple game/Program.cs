a9432cf2e097c803bcaefc40aa55a36b7f6841a0
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-12-03 15:02:19 +0100

    shoot the enemy

--- Simple game/Program.cs
+++ Simple game/Program.cs
@@ -16,109 +16,142 @@ namespace Simple_game
             Rectangle shot = new Rectangle(1000, 1000, 10, 10);
             Rectangle enemy = new Rectangle(0, 0, 20, 20);
             string direction = "up";
+            string screen = "game";
             float shotX = 0;
             float shotY = 0;
             int shotCount = 0;
             int reload = 1;
             bool enemyAlive = true;
+            bool shotAlive = false;
 
 
             Raylib.InitWindow(800, 600, "buck");
+            Raylib.SetTargetFPS(60);
             while (!Raylib.WindowShouldClose())
             {
-
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
-                {
-                    player.y -= 0.1f;
-                    direction = "up";
-                }
-
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
-                {
-                    player.x -= 0.1f;
-                    direction = "left";
-                }
-
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
-                {
-                    player.y += 0.1f;
-                    direction = "down";
-                }
-
-                if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
-                {
-                    player.x += 0.1f;
-                    direction = "right";
-                }
-
-
-                if (enemy.x < player.x)
-                {
-                    enemy.x += enemy.x + 0.1f;
-                }
-
-                if (enemy.x > player.x)
-                {
-                    enemy.x -= enemy.x - 0.1f;
-                }
-
-                reload--;
-                shot.x += shotX;
-                shot.y += shotY;
-
                 Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.WHITE);
-                Raylib.DrawRectangleRec(player, Color.RED);
+                // if (screen == "intro")
+                // {
+                //     Raylib.ClearBackground(Color.GRAY);
+                //     Raylib.DrawText("")
+                // }
 
-                if (enemyAlive == true)
+                if (screen == "game")
                 {
-                    Raylib.DrawRectangleRec(enemy, Color.BLUE);
-                }
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && !Raylib.CheckCollisionRecs(player, borderNorth))
+                    {
+                        player.y -= 2.5f;
+                        direction = "up";
+                    }
 
-                if (!Raylib.CheckCollisionRecs(shot, borderNorth) && !Raylib.CheckCollisionRecs(shot, borderWest) && !Raylib.CheckCollisionRecs(shot, borderSouth) && !Raylib.CheckCollisionRecs(shot, borderEast))
-                {
-                    Raylib.DrawRectangleRec(shot, Color.GREEN);
-                }
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_A) && !Raylib.CheckCollisionRecs(player, borderWest))
+                    {
+                        player.x -= 2.5f;
+                        direction = "left";
+                    }
 
-                if (Raylib.CheckCollisionRecs(shot, borderNorth) || Raylib.CheckCollisionRecs(shot, borderWest) || Raylib.CheckCollisionRecs(shot, borderSouth) || Raylib.CheckCollisionRecs(shot, borderEast))
-                {
-                    shotCount = 0;
-                }
-                if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "up")
-                {
-                    shot.x = player.x + 5;
-                    shot.y = player.y - 10;
-                    shotX = 0;
-                    shotY = -0.15f;
-                    reload = 1;
-                    shotCount = 1;
-                }
-                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "left")
-                {
-                    shot.x = player.x - 10;
-                    shot.y = player.y + 5;
-                    shotX = -0.15f;
-                    shotY = 0;
-                    reload = 1;
-                    shotCount = 1;
-                }
-                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "down")
-                {
-                    shot.x = player.x + 5;
-                    shot.y = player.y + 20;
-                    shotX = 0;
-                    shotY = 0.15f;
-                    reload = 1;
-                    shotCount = 1;
-                }
-                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "right")
-                {
-                    shot.x = player.x + 20;
-                    shot.y = player.y + 5;
-                    shotX = 0.15f;
-                    shotY = 0;
-                    reload = 1;
-                    shotCount = 1;
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && !Raylib.CheckCollisionRecs(player, borderSouth))
+                    {
+                        player.y += 2.5f;
+                        direction = "down";
+                    }
+
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && !Raylib.CheckCollisionRecs(player, borderEast))
+                    {
+                        player.x += 2.5f;
+                        direction = "right";
+                    }
+
+
+                    if (enemy.x < player.x)
+                    {
+                        enemy.x += 2f;
+                    }
+
+                    if (enemy.x > player.x)
+                    {
+                        enemy.x -= 2f;
+                    }
+
+                    if (enemy.y < player.y)
+                    {
+                        enemy.y += 2f;
+                    }
+
+                    if (enemy.y > player.y)
+                    {
+                        enemy.y -= 2f;
+                    }
+
+                    if (Raylib.CheckCollisionRecs(shot, enemy))
+                    {
+                        enemyAlive = false;
+                        shotCount = 0;
+                    }
+
+                    reload--;
+                    shot.x += shotX;
+                    shot.y += shotY;
+
+                    Raylib.ClearBackground(Color.WHITE);
+                    Raylib.DrawRectangleRec(player, Color.RED);
+
+                    if (enemyAlive == true)
+                    {
+                        Raylib.DrawRectangleRec(enemy, Color.BLUE);
+                    }
+
+                    if (shotAlive == true)
+                    {
+                        Raylib.DrawRectangleRec(shot, Color.GREEN);
+                    }
+
+                    if (Raylib.CheckCollisionRecs(shot, borderNorth) || Raylib.CheckCollisionRecs(shot, borderWest) || Raylib.CheckCollisionRecs(shot, borderSouth) || Raylib.CheckCollisionRecs(shot, borderEast) || Raylib.CheckCollisionRecs(shot, enemy))
+                    {
+                        shotCount = 0;
+                        shotAlive = false;
+                    }
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "up")
+                    {
+                        shot.x = player.x + 5;
+                        shot.y = player.y - 10;
+                        shotX = 0;
+                        shotY = -3.5f;
+                        reload = 1;
+                        shotCount = 1;
+                        shotAlive = true;
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "left")
+                    {
+                        shot.x = player.x - 10;
+                        shot.y = player.y + 5;
+                        shotX = -3.5f;
+                        shotY = 0;
+                        reload = 1;
+                        shotCount = 1;
+                        shotAlive = true;
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "down")
+                    {
+                        shot.x = player.x + 5;
+                        shot.y = player.y + 20;
+                        shotX = 0;
+                        shotY = 3.5f;
+                        reload = 1;
+                        shotCount = 1;
+                        shotAlive = true;
+                    }
+                    else if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && shotCount == 0 && reload < 0 && direction == "right")
+                    {
+                        shot.x = player.x + 20;
+                        shot.y = player.y + 5;
+                        shotX = 3.5f;
+                        shotY = 0;
+                        reload = 1;
+                        shotCount = 1;
+                        shotAlive = true;
+                    }
                 }
                 Raylib.EndDrawing();
 
