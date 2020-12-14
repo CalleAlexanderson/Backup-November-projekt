763d324189769e68b1306748c8fd3fec9e47de45
Calle <calle.alexanderson@elev.ga.ntig.se>
2020-12-14 09:38:58 +0100

    waves working

--- Simple game/Program.cs
+++ Simple game/Program.cs
@@ -31,7 +31,10 @@ namespace Simple_game
             bool enemyBossAlive = true;
             bool shotAlive = false;
             bool powerUpAlive = true;
-            int[] enemies = new int[9];
+            bool toggle2 = true;
+            bool toggle3 = true;
+            bool toggle4 = true;
+            bool toggle5 = true;
             Rectangle player = new Rectangle(400, 300, 20, 20);
             Rectangle borderNorth = new Rectangle(0, 0, 800, 1);
             Rectangle borderWest = new Rectangle(0, 0, 1, 600);
@@ -39,16 +42,16 @@ namespace Simple_game
             Rectangle borderEast = new Rectangle(799, 0, 1, 600);
             Rectangle shot = new Rectangle(1000, 1000, shotSizeX, shotSizeY);
             Rectangle enemy1 = new Rectangle(0, 0, 20, 20);
-            Rectangle enemy2 = new Rectangle(0, 0, 20, 20);
-            Rectangle enemy3 = new Rectangle(780, 580, 20, 20);
-            Rectangle enemy4 = new Rectangle(0, 0, 20, 20);
-            Rectangle enemy5 = new Rectangle(780, 580, 20, 20);
-            Rectangle enemy6 = new Rectangle(0, 0, 20, 20);
-            Rectangle enemy7 = new Rectangle(780, 585, 20, 20);
-            Rectangle enemyS1 = new Rectangle(785, 0, 15, 15);
-            Rectangle enemyS2 = new Rectangle(785, 0, 15, 15);
-            Rectangle enemyS3 = new Rectangle(0, 585, 15, 15);
-            Rectangle enemyBoss = new Rectangle(0, 0, 40, 40);
+            Rectangle enemy2 = new Rectangle(-100, 0, 20, 20);
+            Rectangle enemy3 = new Rectangle(-100, 580, 20, 20);
+            Rectangle enemy4 = new Rectangle(-100, 0, 20, 20);
+            Rectangle enemy5 = new Rectangle(-100, 580, 20, 20);
+            Rectangle enemy6 = new Rectangle(-100, 0, 20, 20);
+            Rectangle enemy7 = new Rectangle(-100, 580, 20, 20);
+            Rectangle enemyS1 = new Rectangle(-100, 0, 15, 15);
+            Rectangle enemyS2 = new Rectangle(-100, 0, 15, 15);
+            Rectangle enemyS3 = new Rectangle(-100, 585, 15, 15);
+            Rectangle enemyBoss = new Rectangle(-150, 0, 40, 40);
             Rectangle powerUp = new Rectangle(140, 450, 15, 15);
 
             Raylib.InitWindow(800, 600, "Game");
@@ -92,46 +95,6 @@ namespace Simple_game
                     {
                         player.x += 2.5f;
                         direction = "right";
-                    }
-
-                    if (enemy1.x < player.x)
-                    {
-                        enemy1.x += 2f;
-                    }
-
-                    if (enemy1.x > player.x)
-                    {
-                        enemy1.x -= 2f;
-                    }
-
-                    if (enemy1.y < player.y)
-                    {
-                        enemy1.y += 2f;
-                    }
-
-                    if (enemy1.y > player.y)
-                    {
-                        enemy1.y -= 2f;
-                    }
-
-                    if (enemy2.x < player.x)
-                    {
-                        enemy2.x += 2f;
-                    }
-
-                    if (enemy2.x > player.x)
-                    {
-                        enemy2.x -= 2f;
-                    }
-
-                    if (enemy2.y < player.y)
-                    {
-                        enemy2.y += 2f;
-                    }
-
-                    if (enemy2.y > player.y)
-                    {
-                        enemy2.y -= 2f;
                     }
 
                     if (Raylib.CheckCollisionRecs(shot, enemy1))
@@ -215,56 +178,320 @@ namespace Simple_game
                     if (enemy1Alive == true && wave == 1)
                     {
                         Raylib.DrawRectangleRec(enemy1, Color.BLUE);
+
+                        if (enemy1.x < player.x)
+                        {
+                            enemy1.x += 2f;
+                        }
+
+                        if (enemy1.x > player.x)
+                        {
+                            enemy1.x -= 2f;
+                        }
+
+                        if (enemy1.y < player.y)
+                        {
+                            enemy1.y += 2f;
+                        }
+
+                        if (enemy1.y > player.y)
+                        {
+                            enemy1.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy1.x = -100;
                     }
 
                     if (enemy2Alive == true && wave == 2)
                     {
                         Raylib.DrawRectangleRec(enemy2, Color.BLUE);
+                        if (enemy2.x < player.x)
+                        {
+                            enemy2.x += 2f;
+                        }
+
+                        if (enemy2.x > player.x)
+                        {
+                            enemy2.x -= 2f;
+                        }
+
+                        if (enemy2.y < player.y)
+                        {
+                            enemy2.y += 2f;
+                        }
+
+                        if (enemy2.y > player.y)
+                        {
+                            enemy2.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy2.x = -100;
                     }
 
                     if (enemy3Alive == true && wave == 2)
                     {
                         Raylib.DrawRectangleRec(enemy3, Color.BLUE);
+
+                        if (enemy3.x < player.x)
+                        {
+                            enemy3.x += 2f;
+                        }
+
+                        if (enemy3.x > player.x)
+                        {
+                            enemy3.x -= 2f;
+                        }
+
+                        if (enemy3.y < player.y)
+                        {
+                            enemy3.y += 2f;
+                        }
+
+                        if (enemy3.y > player.y)
+                        {
+                            enemy3.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy3.x = -100;
                     }
 
                     if (enemy4Alive == true && wave == 3)
                     {
                         Raylib.DrawRectangleRec(enemy4, Color.BLUE);
+                        if (enemy4.x < player.x)
+                        {
+                            enemy4.x += 2f;
+                        }
+
+                        if (enemy4.x > player.x)
+                        {
+                            enemy4.x -= 2f;
+                        }
+
+                        if (enemy4.y < player.y)
+                        {
+                            enemy4.y += 2f;
+                        }
+
+                        if (enemy4.y > player.y)
+                        {
+                            enemy4.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy4.x = -100;
                     }
 
                     if (enemy5Alive == true && wave == 3)
                     {
                         Raylib.DrawRectangleRec(enemy5, Color.BLUE);
+                        if (enemy5.x < player.x)
+                        {
+                            enemy5.x += 2f;
+                        }
+
+                        if (enemy5.x > player.x)
+                        {
+                            enemy5.x -= 2f;
+                        }
+
+                        if (enemy5.y < player.y)
+                        {
+                            enemy5.y += 2f;
+                        }
+
+                        if (enemy5.y > player.y)
+                        {
+                            enemy5.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy5.x = -100;
                     }
 
                     if (enemy6Alive == true && wave == 4)
                     {
                         Raylib.DrawRectangleRec(enemy6, Color.BLUE);
+                        if (enemy6.x < player.x)
+                        {
+                            enemy6.x += 2f;
+                        }
+
+                        if (enemy6.x > player.x)
+                        {
+                            enemy6.x -= 2f;
+                        }
+
+                        if (enemy6.y < player.y)
+                        {
+                            enemy6.y += 2f;
+                        }
+
+                        if (enemy6.y > player.y)
+                        {
+                            enemy6.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy6.x = -100;
                     }
 
                     if (enemy7Alive == true && wave == 4)
                     {
                         Raylib.DrawRectangleRec(enemy7, Color.BLUE);
+                        if (enemy7.x < player.x)
+                        {
+                            enemy7.x += 2f;
+                        }
+
+                        if (enemy7.x > player.x)
+                        {
+                            enemy7.x -= 2f;
+                        }
+
+                        if (enemy7.y < player.y)
+                        {
+                            enemy7.y += 2f;
+                        }
+
+                        if (enemy7.y > player.y)
+                        {
+                            enemy7.y -= 2f;
+                        }
+                    }
+                    else
+                    {
+                        enemy7.x = -100;
                     }
 
                     if (enemyS1Alive == true && wave == 3)
                     {
                         Raylib.DrawRectangleRec(enemyS1, Color.BLUE);
+
+                        if (enemyS1.x < player.x)
+                        {
+                            enemyS1.x += 1.7f;
+                        }
+
+                        if (enemyS1.x > player.x)
+                        {
+                            enemyS1.x -= 1.7f;
+                        }
+
+                        if (enemyS1.y < player.y)
+                        {
+                            enemyS1.y += 1.7f;
+                        }
+
+                        if (enemyS1.y > player.y)
+                        {
+                            enemyS1.y -= 1.7f;
+                        }
+                    }
+                    else
+                    {
+                        enemyS1.x = -100;
                     }
 
                     if (enemyS2Alive == true && wave == 4)
                     {
                         Raylib.DrawRectangleRec(enemyS2, Color.BLUE);
+
+                        if (enemyS2.x < player.x)
+                        {
+                            enemyS2.x += 1.7f;
+                        }
+
+                        if (enemyS2.x > player.x)
+                        {
+                            enemyS2.x -= 1.7f;
+                        }
+
+                        if (enemyS2.y < player.y)
+                        {
+                            enemyS2.y += 1.7f;
+                        }
+
+                        if (enemyS2.y > player.y)
+                        {
+                            enemyS2.y -= 1.7f;
+                        }
+                    }
+                    else
+                    {
+                        enemyS2.x = -100;
                     }
 
                     if (enemyS3Alive == true && wave == 4)
                     {
                         Raylib.DrawRectangleRec(enemyS3, Color.BLUE);
+                        if (enemyS3.x < player.x)
+                        {
+                            enemyS3.x += 1.7f;
+                        }
+
+                        if (enemyS3.x > player.x)
+                        {
+                            enemyS3.x -= 1.7f;
+                        }
+
+                        if (enemyS3.y < player.y)
+                        {
+                            enemyS3.y += 1.7f;
+                        }
+
+                        if (enemyS3.y > player.y)
+                        {
+                            enemyS3.y -= 1.7f;
+                        }
+                    }
+                    else
+                    {
+                        enemyS3.x = -100;
                     }
 
                     if (enemyBossAlive == true && wave == 5)
                     {
                         Raylib.DrawRectangleRec(enemyBoss, Color.BLUE);
+                    }
+
+                    if (wave == 2 && toggle2)
+                    {
+                        enemy2.x = 0;
+                        enemy3.x = 780;
+                        toggle2 = false;
+                    }
+
+                    if (wave == 3 && toggle3)
+                    {
+                        enemy4.x = 0;
+                        enemy5.x = 780;
+                        enemyS1.x = 785;
+                        toggle3 = false;
+                    }
+
+                    if (wave == 4 && toggle4)
+                    {
+                        enemy6.x = 0;
+                        enemy7.x = 780;
+                        enemyS2.x = 785;
+                        enemyS3.x = 0;
+                        toggle4 = false;
+                    }
+
+                    if (wave == 5 && toggle5)
+                    {
+                        enemyBoss.x = 0;
+                        toggle5 = false;
                     }
 
                     if (shotAlive == true)
@@ -329,17 +556,17 @@ namespace Simple_game
                         wave = 3;
                     }
 
-                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy6Alive == false && enemyS1Alive == false)
+                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy5Alive == false && enemyS1Alive == false)
                     {
                         wave = 4;
                     }
 
-                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy6Alive == false && enemy7Alive == false && enemy7Alive == false && enemyS1Alive == false && enemyS2Alive == false && enemyS3Alive == false)
+                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy5Alive == false && enemy6Alive == false && enemy7Alive == false && enemyS1Alive == false && enemyS2Alive == false && enemyS3Alive == false)
                     {
                         wave = 5;
                     }
 
-                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy6Alive == false && enemy7Alive == false && enemy7Alive == false && enemyS1Alive == false && enemyS2Alive == false && enemyS3Alive == false && enemyBossAlive == false)
+                    if (enemy1Alive == false && enemy2Alive == false && enemy3Alive == false && enemy4Alive == false && enemy5Alive && enemy6Alive == false && enemy7Alive == false && enemyS1Alive == false && enemyS2Alive == false && enemyS3Alive == false && enemyBossAlive == false)
                     {
                         screen = "win";
                     }
@@ -347,9 +574,8 @@ namespace Simple_game
 
                     if (wave > 1 && powerUpAlive == true)
                     {
-                        Raylib.DrawRectangleRec(powerUp, Color.BLUE);
+                        Raylib.DrawRectangleRec(powerUp, Color.YELLOW);
                     }
-
                 }
                 Raylib.EndDrawing();
 
